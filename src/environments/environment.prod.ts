111a60c0d952b3cafb4340e896fa567df64a52f7
ramich2018 <ramichpro@gmail.com>
2020-01-27 13:02:30 +0100

    tttt1234

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   expirationTime : 5000,
-  backend: 'https://testramich2020.herokuapp.com/' 
+  backend: 'https://testramich2020.herokuapp.com' 
 };
